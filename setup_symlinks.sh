4f23f868709767ffb2c85999fde7dfb01823a46d
Samuel John <github@SamuelJohn.de>
2013-12-09 18:49:52 +0100

    initial buggy version of setup_symlinks.sh

---
+++ setup_symlinks.sh
@@ -0,0 +1,15 @@
+
+
+settings_dir = "~/Dropbox/__settings__"
+
+cp "${settings_dir}/checksha1_copy" "~/checksha1"
+ln -s "${settings_dir}/.zshrc.sh" "~/.zshrc.shared.sh"
+
+
+# Todo:
+# git clone zprezto
+# git pull zprezto (g stash before!)
+# git submodule update
+# link zprezto runcoms
+#
+...
