f52ff953178d62084a3cd89357d103d119efc502
Samuel John <github@SamuelJohn.de>
2014-04-01 12:13:36 +0200

    install/update zprezto

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -8,11 +8,8 @@ ln -s "${settings_dir}/profile.sh" "${HOME}/.profile"
 ln -s "${settings_dir}/common.sh" "${HOME}/.common"
 ln -s "${settings_dir}/vimrc" "${HOME}/.vimrc"
 
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${HOME}/.zprezto"
+cd "${HOME}/.zprezto"
+git pull && git submodule update --init --recursive
+cd $settings_dir
 
-# Todo:
-# git clone zprezto
-# git pull zprezto (g stash before!)
-# git submodule update
-# link zprezto runcoms
-#
-...
